0b4ad18924d2bd163c90fd4b189a8a3d1f8e1b8f
vRogerv <31167228+vRogerv@users.noreply.github.com>
2017-08-20 04:23:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,36 @@
 <?php
   session_start();
   include ('steamauth/userInfo.php');
-
   $servername = "...";
   $username = "...";
   $password = "...";
   $dbname = "...";
   $user = $steamprofile['personaname'];
   $steamid = $steamprofile['steamid'];
-
   $conn = new mysqli($servername, $username, $password, $dbname);
-
   if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
   } 
-
   $sqlget = "SELECT * FROM Players ORDER BY Date";
   $sqldata = mysqli_query($conn, $sqlget) or die('error getting data');
-
   $sqlget2 = "SELECT * FROM Desciption ORDER BY Date DESC LIMIT 1";
   $sqldata2 = mysqli_query($conn, $sqlget2) or die('error getting data');
-
   $sqlget3 = "SELECT * FROM Stream ORDER BY Time DESC LIMIT 3";
   $sqldata3 = mysqli_query($conn, $sqlget3) or die('error getting data');
-
   $sqlget4 = "SELECT COUNT(*) FROM Players";
   $sqldata4= mysqli_query($conn, $sqlget4) or die('error getting data');
-
   $sqlget6 = "SELECT * FROM MasterList WHERE SteamID = '$steamid' LIMIT 1";
   $sqldata6= mysqli_query($conn, $sqlget6) or die('error getting data'); 
-
   $sqlget7 = "SELECT Name FROM Players WHERE Name = '$user' LIMIT 1";
   $sqldata7= mysqli_query($conn, $sqlget7) or die('error getting data'); 
-
   $sqlget8 = "SELECT * FROM Log ORDER BY Time DESC LIMIT 10";
   $sqldata8 = mysqli_query($conn, $sqlget8) or die('error getting data');
-
   $sqlget9 = "SELECT * FROM MasterList WHERE SteamID = '$steamid' LIMIT 1";
   $sqldata9 = mysqli_query($conn, $sqlget9) or die('error getting data');
-
   $sqlget10 = "SELECT * FROM MasterList ORDER BY Coins DESC LIMIT 10";
   $sqldata10 = mysqli_query($conn, $sqlget10) or die('error getting data');
-
   $sqlget11 = "SELECT * FROM MasterList WHERE SteamID = '$steamid' LIMIT 1";
   $sqldata11 = mysqli_query($conn, $sqlget11) or die('error getting data');
-
-
   $conn->close();
 ?>
 <html lang="en">
@@ -59,7 +44,6 @@
       height: auto;
       padding: 10px;
       @include box-sizing(border-box);
-
       &:focus {
         z-index: 2;
       }
@@ -78,7 +62,7 @@
           <a class="navbar-brand" href="index.php">LuisGPlays</a>
           <?php
               if(isset($_SESSION['steamid'])) {
-                    echo "<a href=\"https://www.luisgplays.com/free.php\"><button type=\"button\" class=\"btn btn-success navbar-btn\" style=\"display:inline-block\">Free <i class=\"fa fa-money fa-1x\" aria-hidden=\"true\" style=\"color: white;\"></i></button></a>";     
+                    echo "<a href=\"https://www.luisgplays.com/free.php\" style='margin-left: 10px'><button type=\"button\" class=\"btn btn-success navbar-btn\" style=\"display:inline-block\">Free <i class=\"fa fa-money fa-1x\" aria-hidden=\"true\" style=\"color: white;\"></i></button></a>";     
                   
                 while($row = mysqli_fetch_array($sqldata9, MYSQLI_ASSOC)){
                   echo "<button id=\"myButton\"type=\"button\" class=\"btn btn-primary navbar-btn\" style=\"display:inline-block; margin-left: 10px\">";
@@ -174,40 +158,40 @@
           echo "</div>";
           echo "</div>";
           
-              if($row['Entires'] == 20) {
-               //echo "<div class=\"alert alert-danger\" role=\"alert\">";
-               //echo "<p>Giveaway entries closed! Try another.</p>";
-               //echo "</div>";
-                  
-               }
-              if($row['Entires'] < 20) {
-              
-                  if($row7['Coins'] == 25) {
-                   echo "<form id='name' action='process.php' method='post' accept-charset='UTF-8'>";
-                   echo "<fieldset>";
-                   //echo "<button class=\"btn btn-lg btn-primary btn-block animated zoomIn\" type=\"submit\" value=\"Submit\">";
-                   //echo "Join the giveaway for <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25</button>";
-                   echo "</fieldset>";
-                   echo "</form>"; 
-
-                 }
-                 if($row7['Coins'] > 25){
-                   echo "<form id='name' action='process.php' method='post' accept-charset='UTF-8'>";
-                   echo "<fieldset>";
-                   //echo "<button class=\"btn btn-lg btn-primary btn-block animated zoomIn\" type=\"submit\" value=\"Submit\">";
-                   //echo "Join the giveaway for <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25</button>";
-                   echo "</fieldset>";
-                   echo "</form>";    
-
-                  } 
-
-                 if($row7['Coins'] < 25) {
-                   //echo "<div class=\"alert alert-danger\" role=\"alert\">";
-                   //echo "<p>You need at least <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25 to enter.</p>";
-                   //echo "</div>";
-
-                   }
-               }
+//              if($row['Entires'] == 20) {
+//               echo "<div class=\"alert alert-danger\" role=\"alert\">";
+//               echo "<p>Giveaway entries closed! Try another.</p>";
+//               echo "</div>";
+//                  
+//               }
+//              if($row['Entires'] < 20) {
+//              
+//                  if($row7['Coins'] == 25) {
+//                   echo "<form id='name' action='process.php' method='post' accept-charset='UTF-8'>";
+//                   echo "<fieldset>";
+//                   echo "<button class=\"btn btn-lg btn-primary btn-block animated zoomIn\" type=\"submit\" value=\"Submit\">";
+//                   echo "Join the giveaway for <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25</button>";
+//                   echo "</fieldset>";
+//                   echo "</form>"; 
+//
+//                 }
+//                 if($row7['Coins'] > 25){
+//                   echo "<form id='name' action='process.php' method='post' accept-charset='UTF-8'>";
+//                   echo "<fieldset>";
+//                   echo "<button class=\"btn btn-lg btn-primary btn-block animated zoomIn\" type=\"submit\" value=\"Submit\">";
+//                   echo "Join the giveaway for <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25</button>";
+//                   echo "</fieldset>";
+//                   echo "</form>";    
+//
+//                   } 
+//
+//                 if($row7['Coins'] < 25) {
+//                   echo "<div class=\"alert alert-danger\" role=\"alert\">";
+//                   echo "<p>You need at least <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 25 to enter.</p>";
+//                   echo "</div>";
+//
+//                   }
+//               }
              
             
           echo "</div>";
@@ -226,7 +210,6 @@
         <?php
           echo "<table>";
                 echo "<tr><th style=\"padding: 20px\">Name</th><th style=\"padding: 20px\">Points</th></tr>";
-
                 while($row5 = mysqli_fetch_array($sqldata10, MYSQLI_ASSOC)) {
                   echo "<tr><td style=\"padding: 20px\">";
                   echo $row5['Name'];
@@ -297,9 +280,7 @@
             $row2 = mysqli_fetch_array($sqldata7, MYSQLI_ASSOC);
             $userna = $steamprofile['personaname'];
             $fee = '100';
-
             require 'steamauth/steamauth.php';
-
             if(isset($_SESSION['steamid'])) 
             {
               if($row['Ban'] == '2')
@@ -355,7 +336,6 @@
                 echo "</div>";
                 
                 } 
-
               if($row['Coins'] < 50) {
                 echo "<div class=\"alert alert-danger\" role=\"alert\">";
                 echo "<p>You need at least <i class=\"fa fa-money\" aria-hidden=\"true\"></i> 50 to sign up for the list!</p>";
